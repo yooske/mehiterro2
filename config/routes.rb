dec537f180037276403c4e9ab426c9cadbb9f59c
Yousuke <yooske0214@gmail.com>
2022-09-08 03:46:09 +0000

    [復習]meshiterro2 userモデルにprofile_imageのactivestorage作成,userコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :user, only: [:index, :show, :edit]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
