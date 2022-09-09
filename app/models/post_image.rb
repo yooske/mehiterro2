0cbc977606db75c0ba589d07a35b21c10428a225
Yousuke <yooske0214@gmail.com>
2022-09-09 08:40:57 +0000

    [復習]meshiterro2 コメント機能の作成

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,7 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
 
   validates :shop_name, {presence: true, length: {maximum:20}}
   validates :caption, {presence: true, length: {maximum:60}}
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
