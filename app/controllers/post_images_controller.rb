abc3d222e3a5e49585a9591f6d3dd9162a46a26d
Yousuke <yooske0214@gmail.com>
2022-09-10 06:20:31 +0000

    [復習]meshiterro2 フォロー機能の追加,レイアウトの整理

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -20,6 +20,7 @@ class PostImagesController < ApplicationController
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
+    @user = @post_image.user
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show, :edit, :update]
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorites, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+  resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
